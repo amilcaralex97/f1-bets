4b3b99a5dbb3bd57eb94816d3b3495e79d429f10
amilcaralex97 <amilcaralex97@gmail.com>
2022-10-03 23:20:29 -0500

    react wip

---
+++ app/src/components/Navbar/styles/Wrapper.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+export const Wrapper = styled.nav `
+    background-color: #c92118;
+    display: grid;
+    grid-template-columns: 33% 34% 33%;
+    grid-template-rows: 100%;
+`;
+
+export const List = styled.ul `
+    
+`
